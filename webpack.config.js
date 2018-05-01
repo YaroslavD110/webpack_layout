36c5811ed1b7498c428a50d1ce8aebcbf84181ab
tangerine69 <mrtangerine69@gmail.com>
2018-05-01 12:00:05 +0300

    v1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,34 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+const extractSassConsfig =
+    ExtractTextPlugin.extract({
+        use: [
+            {
+                loader: 'css-loader',
+                options: {
+                    minimize: true,
+                    sourceMap: true,
+                }
+            },
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true,
+                    plugins: [
+                        require('autoprefixer')
+                    ]
+                }
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                }
+            },
+        ]
+    });
 
 const config = {
     entry: './src/index.js',
@@ -11,50 +40,38 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: 'node_modules',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true,
-                                sourceMap: true,
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true,
-                                plugins: [
-                                    require('autoprefixer')
-                                ]
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true,
-                            }
-                        },
-                    ]
-                }),
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+
             }
         ]
     },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
     plugins: [
         new ExtractTextPlugin('css/main.min.css'),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
         contentBase: __dirname,
         compress: true,
         port: 3000,
+        hot: true,
     },
     devtool: "overlay",
 };
 
-module.exports = config;
+module.exports = (env, options) => {
+    if( options.mode !== 'development' ) {
+        config.module.rules[1].use = extractSassConsfig;
+    }
+
+    return config;
+};
